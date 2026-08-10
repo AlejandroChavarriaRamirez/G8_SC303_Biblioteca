ae2a3a1a304a5e944c91e1062ec8de4bb6f2a776
Alejandro Chavarria <alechavarria2022@gmail.com>
2026-08-10 11:35:59 -0600

    Menu

--- src/Biblioteca/Principal.java
+++ src/Biblioteca/Principal.java
@@ -15,6 +15,9 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Menu m = new Menu();
+        m.setVisible(true);
+        m.setLocationRelativeTo(null);
     }
-    
+
 }
