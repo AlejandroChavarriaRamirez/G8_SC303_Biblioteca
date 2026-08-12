3924aa19782a28b0a9f6c51c4818286748401414
Alejandro Chavarria <alechavarria2022@gmail.com>
2026-08-12 11:27:08 -0600

    Agrego gif

--- src/Biblioteca/Menu.java
+++ src/Biblioteca/Menu.java
@@ -64,7 +64,7 @@ public class Menu extends javax.swing.JFrame {
     private void initComponents() {
 
         jMenuItem1 = new javax.swing.JMenuItem();
-        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         Jmenu1 = new javax.swing.JMenu();
         mnuUsuario = new javax.swing.JMenuItem();
@@ -82,18 +82,7 @@ public class Menu extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(0, 102, 102));
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 218, Short.MAX_VALUE)
-        );
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/LibraryScene_Final_Animated.gif"))); // NOI18N
 
         Jmenu1.setText("Archivos");
 
@@ -145,11 +134,11 @@ public class Menu extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabel1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabel1)
         );
 
         pack();
@@ -268,16 +257,16 @@ public class Menu extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu Jmenu1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JMenuItem mnuBuscarLibro;
-    private javax.swing.JMenuItem mnuDevolucion;
+    public javax.swing.JMenuItem mnuDevolucion;
     private javax.swing.JMenuItem mnuHistorial;
     public javax.swing.JMenuItem mnuLibro;
-    private javax.swing.JMenuItem mnuMulta;
-    private javax.swing.JMenuItem mnuPrestamo;
-    private javax.swing.JMenuItem mnuReporte;
+    public javax.swing.JMenuItem mnuMulta;
+    public javax.swing.JMenuItem mnuPrestamo;
+    public javax.swing.JMenuItem mnuReporte;
     private javax.swing.JMenuItem mnuReportes;
     public javax.swing.JMenuItem mnuReserva;
     public javax.swing.JMenuItem mnuUsuario;
